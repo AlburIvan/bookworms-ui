7121c8335495fc79f3f8f348c13d9c12727bdc8f
Ivan Alburquerque <albur.ivan@outlook.com>
2019-04-15 02:23:46 -0400

    Finish 1st version

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-let nav = document.querySelector('nav');
-let burgerMenu = document.querySelector('.header-search__menu');
+let nav             = document.querySelector('nav');
+let burgerMenu      = document.querySelector('.header-search__menu');
 let burgerMenuClose = document.querySelector('.close');
-let overlay = document.querySelector('.overlay');
+let overlay         = document.querySelector('.overlay');
 
 
 burgerMenu.addEventListener('click', () => {
     nav.classList.add('open')
     overlay.classList.add('show')
+    document.body.classList.add('no-scrollable');
 });
 
 burgerMenuClose.addEventListener('click', () => {
-    nav.classList.remove('open')
-    overlay.classList.remove('show')
+    nav.classList.remove('open');
+    overlay.classList.remove('show');
+    document.body.classList.remove('no-scrollable');
 });
 
 overlay.addEventListener('click', () => {
-    nav.classList.remove('open')
-    overlay.classList.remove('show')
+    nav.classList.remove('open');
+    overlay.classList.remove('show');
+    document.body.classList.remove('no-scrollable');
 });
-
